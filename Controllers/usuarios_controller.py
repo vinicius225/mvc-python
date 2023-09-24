febfbca5e11769347b9e4914fc477da22c0496d1
= <=>
2023-09-24 00:20:00 -0300

    ajustes na validacao do token jwt

--- Controllers/usuarios_controller.py
+++ Controllers/usuarios_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Header
 from Aplications.Services.usuarios_service import UsuariosServices
 from Aplications.Services.autenticacao_service import AutenticacaoService
 rotas = APIRouter()
@@ -7,8 +7,13 @@ import json
 usuario_services = UsuariosServices()
 autenticacao_services = AutenticacaoService()
 @rotas.get('/usuario')
-def listar_usuarios(usuario : bool = Depends(autenticacao_services.decode_token)):
-    if usuario:
-        retorno =  usuario_services.listar_usuarios()
-        return retorno
-    return   HTTPException(status_code=401, detail= "Token invalido")
+def listar_usuarios(authorization  = Header(None)):
+    if authorization is None:
+        raise HTTPException(status_code=401, detail="Autenticação JWT obrigatória")
+    token = authorization.split("Bearer ")[1] if authorization.startswith("Bearer ") else None
+    if token is None:
+        raise HTTPException(status_code=401, detail="Token JWT inválido")
+    user = autenticacao_services.decode_token(token)  # Substitua pela sua função de decodificação de token JWT
+    if user == False:
+        raise HTTPException(status_code=401, detail="Token JWT inválido")
+    return user
